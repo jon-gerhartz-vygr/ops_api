56beb030fdb4f38d1f06be3cd1e8c2ccce4bf6c6
jon-gerhartz-vygr <jgerhartz@investvoyager.com>
2024-03-27 11:47:29 -0400

    Added handling of bad keys, added updates to updated_ts, added docs to read.me

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # ops_api
+
+API for Remote Updates to Snowflake Data
+
+## Authentication
+
+Authenticate your request by passing your API key as a bearer token in the authorization header
+
+**Sample headers**
+
+```
+{'Authorization': 'Bearer <api_key>'}
+```
+
+## Endpoints
+
+**/update-user**
+
+**Description:**
+
+Use this endpoint to update any column in the user table. Successfully calling this endpoint will directly update data in the user table and create a log record in the audit log events table.
+
+**Required Args:**
+
+- user_id - id from users table or voyager user_id
+- data - JSON payload of updates to make. Keys should be column name (case insensitive) values should be new value to update to. All keys passed will attempt to be updated. If you pass a key that is not a column in the user table, the request will fail. Values are case sensative and will be updated exactly as passed. Be sure that the value passed for an update matches the format of the column that you are attempting an update on. Pass all values as strings.
+
+**Sample Request**
+
+Curl
+
+```
+url -X POST -H "Authorization: Bearer <api_key>" -d '{"state": "NJ", "city": "Newark", "zip": "08906"}' https://opsapi-production.up.railway.app/update_user?user_id=123456
+```
+
+Python
+
+```
+import requests
+
+user_id = 123456
+url = f'https://opsapi-production.up.railway.app/update_user?user_id={user_id}'
+
+api_key = '123456789'
+headers = {
+    'Authorization': f'Bearer {api_key}'
+}
+
+payload = {"state": "NJ", "city": "Newark", "zip": "08906"}
+
+resp = request.post(url, headers=headers, json=paylod)
+```
+
+**Accepted Values**
+
+- EMAIL
+- FIRST_NAME
+- LAST_NAME
+- ADDRESS1
+- ADDRESS2
+- CITY
+- STATE
+- ZIP
+- PHONE_COUNTRY_CODE
+- PHONE
+- COUNTRY_CODE
+- IS_FOREIGN
+- HAS_CLAIM
--- crud.py
+++ crud.py
@@ -1,25 +1,58 @@
 from connect import execute_query, fetch_pandas
 from queries import *
 
+allowed_keys = {'EMAIL', 'FIRST_NAME', 'LAST_NAME', 'ADDRESS1', 'ADDRESS2', 'CITY',
+                'STATE', 'ZIP', 'PHONE_COUNTRY_CODE', 'PHONE', 'COUNTRY_CODE', 'IS_FOREIGN', 'HAS_CLAIM'}
+
+address_fields = ['ADDRESS1', 'ADDRESS2', 'CITY',
+                  'STATE', 'ZIP', 'COUNTRY_CODE']
+
+name_fields = ['FIRST_NAME', 'LAST_NAME']
+
 
 def update_user(user_id, data):
     len_data = len(data)
     update_statements = ''
     i = 0
     keys = []
+    update_name = False
+    update_address = False
+    update_email = False
+
     for key in data:
-        keys.append(key.upper())
+        upper_key = key.upper()
+        keys.append(upper_key)
         statement = f"{key}='{data[key]}'"
         if len_data != 1:
             if i != len_data - 1:
                 statement += ','
         update_statements += statement
+
+        if upper_key in address_fields:
+            update_address = True
+        elif upper_key in name_fields:
+            update_name = True
+        elif upper_key == 'EMAIL':
+            update_email = True
+
         i += 1
 
-    formatted_q_get_user = q_get_user.format(user_id=user_id)
+    if update_address == True:
+        update_statements += ', ADDRESS_UPDATED_TS = CURRENT_TIMESTAMP'
+    if update_name == True:
+        update_statements += ', NAME_UPDATED_TS = CURRENT_TIMESTAMP'
+    if update_email == True:
+        update_statements += ', EMAIL_UPDATED_TS = CURRENT_TIMESTAMP'
 
+    formatted_q_get_user = q_get_user.format(user_id=user_id)
+    upper_data_keys = set([key.upper() for key in data.keys()])
     try:
         current_user_data_df = fetch_pandas(formatted_q_get_user)
+
+        unallowed_keys = upper_data_keys - allowed_keys
+        assert len(
+            unallowed_keys) == 0, f'Invalid keys passed: {str(unallowed_keys)}'
+
         prior_pii_data = current_user_data_df[keys].to_dict(orient='records')[
             0]
 
